dca789f3ad3e4dc7de9a661b117fd126120fe6ac
Sølve Tornøe <solve@tornoee.com>
2021-10-13 20:34:43 +0200

    Seperate JSON to individual files and fetch them

--- main.js
+++ main.js
@@ -1,112 +1,22 @@
-let consume = [
-    {
-        "url": "https://www.youtube.com/watch?v=iZWp2VNfxfU&list=PL5vZEY2A9f_jEe0y_6MGmcsaMIa1gO3wU",
-        "text": "Watch an ant colony grow"
-    },
-    {
-        "url": "https://www.youtube.com/watch?v=Gtz18bDP0_w",
-        "text": "Baking ASMR by SEODAM"
-    },
-    {
-        "url": "https://www.youtube.com/watch?v=UjtOGPJ0URM&list=PLFs4vir_WsTzcfD7ZE8uO3yX-GCKUk9xZ",
-        "text": "Learn interesting science bits from Kurstgezagt"
-    },
-    "Sit still with eyes closed and listen to your surroundings for 5 minutes",
-    {
-        "url": "https://www.youtube.com/watch?v=jGwh-vvDwUw&list=PLoZ-LEVLUe6b8NamywcV2pai_pqVGn5zs",
-        "text": "Watch paintings get restored by Baumgartner"
-    },
-    {
-        "url": "https://xkcd.com",
-        "text": "View latest comic from XKCD"
-    },
-    {
-        "url": "https://asoftmurmur.com",
-        "text": "Listen to soft background noises in asoftmurmur"
-    },
-    {
-        "url": "https://www.theonion.com",
-        "text": "Read funny fake articles from The Onion"
-    },
-    {
-        "url": "https://www.supercook.com/#/recipes",
-        "text": "Find recipes from stuff in your fridge with Supercook"
-    },
-    {
-        "url": "https://www.amazon.com/gp/feature.html?ie=UTF8&docId=1001250201",
-        "text": "Read funny Amazon reviews"
-    }
-]
-let code = [
-    {
-        "url": "https://flukeout.github.io",
-        "text": "Go through the basics of CSS with CssDiner"
-    },
-    {
-        "url": "https://flexboxzombies.com/p/flexbox-zombies",
-        "text": "Learn the basics for css flex with Flexbox Zombies"
-    },
-    {
-        "url": "https://codepen.io/picks/pens",
-        "text": "Browse through cool code snippets on Codepen"
-    }
-]
+let consume, code, life
 
-// content - do
-let life = [
-    "Find 2 things to put in trash bin",
-    "Go for a walk around the neighborhood",
-    "Call a relative or friend",
-    "Set electronics aside for 10 minutes",
-    "Look for new growth in a nearby plant",
-    "Drink a cup of water",
-    "Clean out your car",
-    "Match storage containers with their lids",
-    "Surprise your significant other with something considerate",
-    "Stretch for 5 minutes",
-    "Visit a nearby museum",
-    "Listen to music you haven't listened to in a while",
-    "Learn how to fold a paper crane",
-    "Learn how to write in shorthand",
-    "Learn how to french braid hair",
-    "Take out the trash",
-    "Find a new recipe to try",
-    "Volunteer hours at the nearest animal shelter",
-    "Do a load of laundry",
-    "Unsubscribe from automated emails in your inbox",
-    "Update your résumé",
-    "Take a bubble bath",
-    "Use your fingers to paint along with Bob Ross",
-    "Doodle an object nearby",
-    "Take a power nap",
-    "Write a letter for future self",
-    "Clean clutter off your desk",
-    "CLose your eyes and listen to the sounds around you for 5 minutes",
-    "Plan a weekend trip to a nearby city",
-    "Write down 10 things (or people) you're grateful for",
-    "Change up the deccor somewhere in your home",
-    "Browse a cookbook for yummy recipes",
-    "Send a handwritten note",
-    "Recreate your favorite restaurant meal",
-    "Recreate your favorite fast food meal",
-    "Visit a nearby animal shelter",
-    "Put together a care package for someone you love",
-    "Start a garden with a random seed you found",
-    "Clean out your medicing cabinet",
-    "Go to the library",
-    "Go through the photos on your phone",
-    "Visit a thrift shop",
-    "Organize the apps on your phone",
-    "Plan your next date",
-    "Visit a nearby museum",
-    "Learn how to roller skate",
-    "Bake a cake",
-    "Go to an estate sale",
-    "Watch the sunset",
-    "Find  nearby farmer's market",
-    "Bake bread"
+/**
+ * Helper function to fetch JSON.
+ * @param {string} path path/url to JSON resource
+ * @returns JSON
+ */
+const fetchJson = async (path) => {
+    const response = await fetch(path)
+    const data = await response.json()
 
-]
+    return data;
+}
+
+// Fetch from JSON
+fetchJson('./db/consume.json').then(data => consume = data.consume)
+fetchJson('./db/code.json').then(data => code = data.code)
+fetchJson('./db/life.json').then(data => life = data.life)
+
 
 function getNewThing(){
     var pickFrom = []
